4675bfd2c28902f6eeb7ba02541bca600950055f
jothi1991 <jothi.lakshmi01@gmail.com>
2022-03-29 19:43:40 -0500

    hashmap

---
+++ src/newpackage/Hashmap.java
@@ -0,0 +1,10 @@
+package newpackage;
+
+public class Hashmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("this is for Hashmap");
+	}
+
+}
