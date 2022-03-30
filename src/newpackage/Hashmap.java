0ff6f502c578d18b95ed2d5859a08f01e4777df0
jothi1991 <jothi.lakshmi01@gmail.com>
2022-03-29 20:18:33 -0500

    hashmap added

---
+++ src/newpackage/Hashmap.java
@@ -0,0 +1,11 @@
+package newpackage;
+
+public class Hashmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println(" new hashmap file");
+          System.out.println("changes in newfile");
+	}
+
+}
